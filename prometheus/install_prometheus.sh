2a33d7e84c3c44432d783c5b0f240847f212d203
Raju Bhattarai <rbhattarai525@gmail.com>
2025-12-23 09:18:18 +0545

    file[install_promethus.sh]: New file upload

---
+++ prometheus/install_prometheus.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+PROMETHEUS_VERSION="v3.5.0git "
+PROMETHEUS_FILE="prometheus-3.50.linux-amd64"
+
+sudo useradd --no-create-home --shell /bin/false prometheus
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
+sudo chown prometheus:prometheus /etc/prometheus
+sudo chown prometheus:prometheus /var/lib/prometheus
+wget https://github.com/prometheus/prometheus/releases/download/"${PROMETHEUS_VERSION}"/"${PROMETHEUS_FILE}".tar.gz
+tar xvf "${PROMETHEUS_FILE}".tar.gz
+cd "${PROMETHEUS_FILE}"
+
+sudo cp prometheus /usr/local/bin/
+sudo cp promtool /usr/local/bin
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo cp prometheus.yml /etc/prometheus/prometheus.yml
+sudo chown prometheus:prometheus /etc/prometheus/prometheus.yml
+cd -
+sudo cp prometheus.service /etc/systemd/system/prometheus.service
+sudo systemctl daemon-reload
+
+sudo systemctl start prometheus
+sudo systemctl enable prometheus
+sudo systemctl status prometheus
