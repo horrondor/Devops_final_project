02842093b57efa0740c8458c5349cfb3b205e0e9
Raju Bhattarai <rbhattarai525@gmail.com>
2025-12-30 10:19:30 +0545

    file[alertmanager]: sudo remove for ansible from  script

--- alertmanager/install_alertmanager.sh
+++ alertmanager/install_alertmanager.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ALERTMANAGER_VERSION="v0.30.0"
-ALERTMANAGER_FILE="alertmanager-0.30.0.linux-amd64.tar.gz"
+ALERTMANAGER_FILE="alertmanager-0.30.0.linux-amd64"
 
 
-sudo useradd --no-create-home --shell /bin/false alertmanager
-sudo mkdir /etc/alertmanager
+useradd --no-create-home --shell /bin/false alertmanager
+mkdir /etc/alertmanager
 
 
 wget https://github.com/prometheus/alertmanager/releases/download/"${ALERTMANAGER_VERSION}"/"${ALERTMANAGER_FILE}".tar.gz
@@ -12,14 +12,14 @@ wget https://github.com/prometheus/alertmanager/releases/download/"${ALERTMANAGE
 tar xzf "${ALERTMANAGER_FILE}".tar.gz
 cd "${ALERTMANAGER_FILE}"
 
-sudo mv alertmanager.yml /etc/alertmanager
-sudo chown -R alertmanager:alertmanager /etc/alertmanager
-sudo mkdir /var/lib/alertmanager
-sudo chown -R alertmanager:alertmanager /var/lib/alertmanager
-sudo cp alertmanager /usr/local/bin
-sudo cp amtool /usr/local/bin
-sudo chown alertmanager:alertmanager /usr/local/bin/alertmanager
-sudo chown alertmanager:alertmanager /usr/local/bin/amtool
+ mv alertmanager.yml /etc/alertmanager
+chown -R alertmanager:alertmanager /etc/alertmanager
+mkdir /var/lib/alertmanager
+chown -R alertmanager:alertmanager /var/lib/alertmanager
+cp alertmanager /usr/local/bin
+cp amtool /usr/local/bin
+chown alertmanager:alertmanager /usr/local/bin/alertmanager
+chown alertmanager:alertmanager /usr/local/bin/amtool
 
 # the service will listen on port 9093
 cd -
@@ -43,9 +43,9 @@ Restart=always
 WantedBy=multi-user.target
 EOF
 
-sudo cp alertmanager.service /etc/systemd/system/
+cp alertmanager.service /etc/systemd/system/
 
-sudo systemctl daemon-reload
-sudo systemctl start alertmanager
-sudo systemctl enable alertmanager
-sudo systemctl status alertmanager
+ systemctl daemon-reload
+ systemctl start alertmanager
+ systemctl enable alertmanager
+#sudo systemctl status alertmanager
--- grafana/install_grafana.sh
+++ grafana/install_grafana.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
-
-sudo apt-get update
-sudo apt-get install -y adduser libfontconfig1 musl
+apt-get update
+apt-get install -y adduser libfontconfig1 musl
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise_12.3.1_amd64.deb
 sudo dpkg -i grafana-enterprise_12.3.1_amd64.deb
 
-sudo systemctl daemon-reload
-sudo systemctl enable grafana-server
-sudo systemctl start grafana-server
-sudo systemctl status grafana-server
+systemctl daemon-reload
+systemctl enable grafana-server
+systemctl start grafana-server
+#systemctl status grafana-server
