65f0a4b55b74beecc54fae9b3b3bec3b08e43123
Raju Bhattarai <rbhattarai525@gmail.com>
2025-12-31 10:11:51 +0545

    file[prometheus]: updated

--- prometheus/install_prometheus.sh
+++ prometheus/install_prometheus.sh
@@ -11,15 +11,31 @@ wget https://github.com/prometheus/prometheus/releases/download/"${PROMETHEUS_VE
 tar xvf "${PROMETHEUS_FILE}".tar.gz
 cd "${PROMETHEUS_FILE}"
 
-sudo cp prometheus /usr/local/bin/
+sudo  prometheus /usr/local/bin/
 sudo cp promtool /usr/local/bin
 sudo chown prometheus:prometheus /usr/local/bin/prometheus
 sudo chown prometheus:prometheus /usr/local/bin/promtool
-sudo chown -R prometheus:prometheus /etc/prometheus/consoles
-sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
 sudo cp prometheus.yml /etc/prometheus/prometheus.yml
 sudo chown prometheus:prometheus /etc/prometheus/prometheus.yml
-cd -
+cat <<EOF > prometheus.service
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+ExecStart=/usr/local/bin/prometheus \
+    --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
+
+[Install]
+WantedBy=multi-user.target
+EOF
 sudo cp prometheus.service /etc/systemd/system/prometheus.service
 sudo systemctl daemon-reload
 
