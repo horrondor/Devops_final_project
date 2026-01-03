1dcc8d4712ed6fac0e2abb569ef41756eabd106a
Raju Bhattarai <rbhattarai525@gmail.com>
2026-01-03 22:56:33 +0545

    file[install_grafana]: sudo removed for ansible

--- grafana/install_grafana.sh
+++ grafana/install_grafana.sh
@@ -3,7 +3,7 @@ set -e
 apt-get update
 apt-get install -y adduser libfontconfig1 musl
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise_12.3.1_amd64.deb
-sudo dpkg -i grafana-enterprise_12.3.1_amd64.deb
+dpkg -i grafana-enterprise_12.3.1_amd64.deb
 
 systemctl daemon-reload
 systemctl enable grafana-server
